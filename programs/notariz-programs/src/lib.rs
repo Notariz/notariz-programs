3c8cab2bd5cea2498eab78ebe2aab352ffa09738
Paralogos <none>
2022-02-24 17:59:31 +0100

    Initial commit

--- programs/notariz-programs/src/lib.rs
+++ programs/notariz-programs/src/lib.rs
@@ -5,7 +5,8 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod notariz_programs {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
--- tests/notariz-programs.js
+++ tests/notariz-programs.js
@@ -1,11 +1,10 @@
-const anchor = require('@project-serum/anchor');
+const anchor = require("@project-serum/anchor");
 
-describe('notariz-programs', () => {
-
+describe("notariz-programs", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
-  it('Is initialized!', async () => {
+  it("Is initialized!", async () => {
     // Add your test here.
     const program = anchor.workspace.NotarizPrograms;
     const tx = await program.rpc.initialize();
